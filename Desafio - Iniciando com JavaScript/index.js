028742e4dd2cbc7b24d42cacf6622de8216a1507
Massaroni <49594702+Massaronii@users.noreply.github.com>
2023-12-06 19:14:12 -0300

    Add files via upload

---
+++ Desafio - Iniciando com JavaScript/index.js
@@ -0,0 +1,35 @@
+alert('Hello world')
+let numberOne = Number(2)
+let numberTwo = Number(3)
+
+let sum = alert(numberOne + numberTwo)
+let sub = alert(numberOne - numberTwo)
+let multi = alert(numberOne * numberTwo)
+let div = alert(numberOne / numberTwo)
+
+
+let menssage = 3
+
+if(typeof menssage === "number" ){
+  alert("isto e um numero")
+}else {
+  alert("isso não é um número")
+}
+
+if (typeof menssage === "string") {
+  alert("isto e uma string")
+} else {
+  alert("isso não é uma string")
+}
+
+if (typeof menssage === "boolean") {
+  alert("isto e um objeto")
+} else {
+  alert("isso não é um objeto")
+} 
+
+if(numberTwo % 2 == 0) {
+  alert("esse numero e par")
+}else{
+  alert("esse número é impar")
+}
